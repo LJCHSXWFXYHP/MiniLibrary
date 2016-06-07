733c85ce8d93319d64fc73325a29f9f860493c3f
YHP <Vaaaas@DESKTOP-TL3OQBG>
2016-06-07 23:10:59 +0800

    YHP0607.1

---
+++ Manager/LoginForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Manager
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UserNameLbl = new System.Windows.Forms.Label();
+            this.PswdLbl = new System.Windows.Forms.Label();
+            this.UserNameBox = new System.Windows.Forms.TextBox();
+            this.PswdBox = new System.Windows.Forms.TextBox();
+            this.LoginBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // UserNameLbl
+            // 
+            this.UserNameLbl.AutoSize = true;
+            this.UserNameLbl.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.UserNameLbl.Location = new System.Drawing.Point(61, 99);
+            this.UserNameLbl.Name = "UserNameLbl";
+            this.UserNameLbl.Size = new System.Drawing.Size(88, 25);
+            this.UserNameLbl.TabIndex = 0;
+            this.UserNameLbl.Text = "用户名：";
+            // 
+            // PswdLbl
+            // 
+            this.PswdLbl.AutoSize = true;
+            this.PswdLbl.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.PswdLbl.Location = new System.Drawing.Point(61, 177);
+            this.PswdLbl.Name = "PswdLbl";
+            this.PswdLbl.Size = new System.Drawing.Size(87, 25);
+            this.PswdLbl.TabIndex = 1;
+            this.PswdLbl.Text = "密   码：";
+            // 
+            // UserNameBox
+            // 
+            this.UserNameBox.Location = new System.Drawing.Point(155, 103);
+            this.UserNameBox.Name = "UserNameBox";
+            this.UserNameBox.Size = new System.Drawing.Size(171, 21);
+            this.UserNameBox.TabIndex = 2;
+            // 
+            // PswdBox
+            // 
+            this.PswdBox.Location = new System.Drawing.Point(155, 181);
+            this.PswdBox.Name = "PswdBox";
+            this.PswdBox.Size = new System.Drawing.Size(171, 21);
+            this.PswdBox.TabIndex = 3;
+            this.PswdBox.UseSystemPasswordChar = true;
+            // 
+            // LoginBtn
+            // 
+            this.LoginBtn.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.LoginBtn.Location = new System.Drawing.Point(155, 259);
+            this.LoginBtn.Name = "LoginBtn";
+            this.LoginBtn.Size = new System.Drawing.Size(75, 35);
+            this.LoginBtn.TabIndex = 4;
+            this.LoginBtn.Text = "登    陆";
+            this.LoginBtn.UseVisualStyleBackColor = true;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.LoginBtn);
+            this.Controls.Add(this.PswdBox);
+            this.Controls.Add(this.UserNameBox);
+            this.Controls.Add(this.PswdLbl);
+            this.Controls.Add(this.UserNameLbl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "LoginForm";
+            this.Text = "请登录";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label UserNameLbl;
+        private System.Windows.Forms.Label PswdLbl;
+        private System.Windows.Forms.TextBox UserNameBox;
+        private System.Windows.Forms.TextBox PswdBox;
+        private System.Windows.Forms.Button LoginBtn;
+    }
+}
+
---
+++ Manager/ManageForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Manager
+{
+    partial class ManageWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("用户管理");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("图书管理");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("订单管理");
+            this.MenuTV = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // MenuTV
+            // 
+            this.MenuTV.Dock = System.Windows.Forms.DockStyle.Left;
+            this.MenuTV.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.MenuTV.Location = new System.Drawing.Point(0, 0);
+            this.MenuTV.Name = "MenuTV";
+            treeNode4.Name = "UsersManageNode";
+            treeNode4.Text = "用户管理";
+            treeNode5.Name = "BooksManageNode";
+            treeNode5.Text = "图书管理";
+            treeNode6.Name = "OrdersManageNode";
+            treeNode6.Text = "订单管理";
+            this.MenuTV.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode4,
+            treeNode5,
+            treeNode6});
+            this.MenuTV.Size = new System.Drawing.Size(192, 561);
+            this.MenuTV.TabIndex = 1;
+            // 
+            // ManageWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Controls.Add(this.MenuTV);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "ManageWindow";
+            this.Text = "管理";
+            this.Load += new System.EventHandler(this.ManageForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TreeView MenuTV;
+    }
+}
